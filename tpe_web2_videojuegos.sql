e69fd8627bfd6f4aff008117dbe9f5b38298b2da
Mauro Ordoniez <mordoniez@alumnos.exa.unicen.edu.ar>
2026-05-14 21:15:41 -0300

    Se agregaron nuevas comlumnas a las tablas juego y editor en la base de datos

--- tpe_web2_videojuegos.sql
+++ tpe_web2_videojuegos.sql
@@ -1,29 +1,37 @@
--- Created by Redgate Data Modeler (https://datamodeler.redgate-platform.com)
--- Last modification date: 2026-04-30 23:28:30.619
--- tables
--- Table: EDITOR
-CREATE TABLE EDITOR (
-    id_editor int NOT NULL AUTO_INCREMENT,
-    nombre_empresa varchar(50) NOT NULL,
-    pais varchar(20) NOT NULL,
-    sitio_web varchar(100) NOT NULL,
-    CONSTRAINT UK_NOMBRE_EMPRESA_EDITOR UNIQUE (nombre_empresa),
-    CONSTRAINT PK_EDITOR PRIMARY KEY (id_editor)
+create table editor
+(
+    id_editor      int auto_increment
+        primary key,
+    nombre_empresa varchar(50)  not null,
+    pais           varchar(20)  not null,
+    sitio_web      varchar(100) not null,
+    valoracion     int          null,
+    imagen         varchar(400) null,
+    constraint UK_NOMBRE_EMPRESA_EDITOR
+        unique (nombre_empresa),
+    constraint CK_VALORACION_EDITOR
+        check (`valoracion` between 0 and 5)
 );
--- Table: VIDEO_JUEGO
-CREATE TABLE VIDEO_JUEGO (
-    id_juego int NOT NULL AUTO_INCREMENT,
-    titulo varchar(50) NOT NULL,
-    descripcion text NOT NULL,
-    precio decimal(6, 2) NOT NULL,
-    fecha_lanzamiento date NOT NULL,
-    FK_id_editor int NOT NULL,
-    CONSTRAINT PK_VIDEO_JUEGO PRIMARY KEY (id_juego),
-    CONSTRAINT UK_TITULO_JUEGO UNIQUE (titulo),
-    CONSTRAINT CK_PRECIO_POSITIVO CHECK (precio >= 0)
+
+create table video_juego
+(
+    id_juego           int auto_increment
+        primary key,
+    titulo             varchar(50)   not null,
+    descripcion        text          not null,
+    precio             decimal(6, 2) not null,
+    fecha_lanzamiento  date          not null,
+    requisitos_sistema text          null,
+    valoracion         int           null,
+    imagen             varchar(400)  null,
+    FK_id_editor       int           not null,
+    constraint UK_TITULO_JUEGO
+        unique (titulo),
+    constraint VIDEO_JUEGO_EDITOR
+        foreign key (FK_id_editor) references editor (id_editor),
+    constraint CK_PRECIO_POSITIVO
+        check (`precio` >= 0),
+    constraint CK_VALORACION
+        check (`valoracion` between 0 and 5)
 );
--- foreign keys
--- Reference: VIDEO_JUEGO_EDITOR (table: VIDEO_JUEGO)
-ALTER TABLE VIDEO_JUEGO
-ADD CONSTRAINT VIDEO_JUEGO_EDITOR FOREIGN KEY (FK_id_editor) REFERENCES EDITOR (id_editor) ON DELETE RESTRICT;
--- End of file.
+
